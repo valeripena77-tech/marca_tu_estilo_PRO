ee2490a920f469128e05f29b0778adcfeb7c3ddd
yurany-13 <zambranocamcachoyirleyalejandra@gmail.com>
2026-06-01 13:33:18 -0600

    letras

--- src/view/home/index.jsx
+++ src/view/home/index.jsx
@@ -9,7 +9,7 @@ export default function Home() {
         <button className="btn">buscar</button>
       </div>
       <div className="video-seccion">
-        <h2>video de muestra!</h2>
+        <h2>video de muestra</h2>
         <video controls>
           <source src="public/video.mp4" type="video/mp4" video></source>
         </video>
