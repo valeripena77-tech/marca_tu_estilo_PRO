c0f578836e8a4557c0f81f7c9ff64d99d4f98a49
yurany-13 <zambranocamcachoyirleyalejandra@gmail.com>
2026-06-01 15:13:27 -0600

    nada

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import "./App.css";
 import { Head } from "./components/head";
 import { Contents } from "./components/contents";
-import { Footer } from "./components/footer";
+import Footer from "./components/footer";
 
 function App() {
   return (
