aacb5579acb7214cd482a687d33065def9d21025
yurany-13 <zambranocamcachoyirleyalejandra@gmail.com>
2026-07-23 13:02:14 -0600

    nuevo

--- src/view/home/index.jsx
+++ src/view/home/index.jsx
@@ -4,14 +4,38 @@ export default function Home() {
   return (
     <div>
       <div className="hero">
-        <div className="bienvenidos">
+        <div className="tarjeta">
           <nav>
             <a href="">inico </a>
+          </nav>
+        </div>
+        <div className="tarjeta">
+          <nav>
             <a href="">tienda </a>
+          </nav>
+        </div>
+        <div className="tarjeta">
+          <nav>
             <a href="">categorias </a>
-            <a href="">ofertas </a>
+          </nav>
+        </div>
+        <div className="tarjeta">
+          <nav>
+            <a href="">oferta </a>
+          </nav>
+        </div>
+        <div className="tarjeta">
+          <nav>
             <a href="">novedades </a>
+          </nav>
+        </div>
+        <div className="tarjeta">
+          <nav>
             <a href="">mas vendidos </a>
+          </nav>
+        </div>
+        <div className="tarjeta">
+          <nav>
             <a href="">contactos </a>
           </nav>
         </div>
