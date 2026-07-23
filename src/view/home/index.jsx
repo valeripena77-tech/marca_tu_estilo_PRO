d99d61efd0c25caed42646287f35b75e5b2950d5
yurany-13 <zambranocamcachoyirleyalejandra@gmail.com>
2026-07-23 14:05:39 -0600

    cambio

--- src/view/home/index.jsx
+++ src/view/home/index.jsx
@@ -4,37 +4,37 @@ export default function Home() {
   return (
     <div>
       <div className="hero">
-        <div className="tarjeta">
+        <div className="tarjetas">
           <nav>
             <a href="">inico </a>
           </nav>
         </div>
-        <div className="tarjeta">
+        <div className="tarjetas">
           <nav>
             <a href="">tienda </a>
           </nav>
         </div>
-        <div className="tarjeta">
+        <div className="tarjetas">
           <nav>
             <a href="">categorias </a>
           </nav>
         </div>
-        <div className="tarjeta">
+        <div className="tarjetas">
           <nav>
             <a href="">oferta </a>
           </nav>
         </div>
-        <div className="tarjeta">
+        <div className="tarjetas">
           <nav>
             <a href="">novedades </a>
           </nav>
         </div>
-        <div className="tarjeta">
+        <div className="tarjetas">
           <nav>
             <a href="">mas vendidos </a>
           </nav>
         </div>
-        <div className="tarjeta">
+        <div className="tarjetas">
           <nav>
             <a href="">contactos </a>
           </nav>
