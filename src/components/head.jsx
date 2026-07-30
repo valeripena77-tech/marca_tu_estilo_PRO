521eca95d74d1a9c18252de47f4518ea4a72e683
angelmauricio10laguna-gif <angelmauricio10laguna@gmail.com>
2026-07-30 14:57:23 -0600

    nuevo

--- src/components/head.jsx
+++ src/components/head.jsx
@@ -1,19 +1,36 @@
 import * as Icons from "lucide-react";
+import { useState } from "react";
 export function Head() {
+  const [toggle, setToggle] = useState(false);
   return (
-    <header className="header">
-      <div className="conten-logo">
-        <img
-          className="img-logo"
-          src="Blue_Teddy_Bear_Sticker-removebg-preview.png"
-          alt=""
-        />
-        <h1 className="logo"> marca tu estilo pro</h1>
+    <nav x-data="{ isOpen: false }" class="relative bg-white shadow dark:bg-gray-800">
+      <div class="container px-6 py-4 mx-auto md:flex md:justify-between md:items-center">
+        <div class="flex items-center justify-between">
+          <a href="#">
+            <img class="w-auto h-6 sm:h-7" src="https://merakiui.com/images/full-logo.svg" alt="" />
+          </a>
+          <div class="flex lg:hidden">
+            <button onClick={() => setToggle(prev => !prev)} x-cloak type="button" class="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400" aria-label="toggle menu"> {toggle ? <Icons.X /> : <Icons.Menu />}
+            </button>
+          </div>
+        </div>
+
+        <div class="absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-white dark:bg-gray-800 md:mt-0 md:p-0 md:top-0 md:relative md:bg-transparent md:w-auto md:opacity-100 md:translate-x-0 md:flex md:items-center">
+          <div class="hidden  md:flex md:mx-6">
+            <a class="my-2 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0" href="#">Home</a>
+            <a class="my-2 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0" href="#">Shop</a>
+            <a class="my-2 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0" href="#">Contact</a>
+            <a class="my-2 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0" href="#">About</a>
+          </div>
+          {toggle && <div class="flex flex-col md:hidden md:mx-6">
+            <a class="my-2 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0" href="#">Home</a>
+            <a class="my-2 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0" href="#">Shop</a>
+            <a class="my-2 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0" href="#">Contact</a>
+            <a class="my-2 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0" href="#">About</a>
+          </div>}
+
+        </div>
       </div>
-      <div className="buscar">
-        <Icons.Search />
-        <input type="text" placeholder="Buscar..." />
-      </div>
-    </header>
+    </nav >
   );
 }
