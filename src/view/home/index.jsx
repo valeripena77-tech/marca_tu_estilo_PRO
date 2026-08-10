c32f1c2f8907b173dc8b558b3b644ed365a74e05
yurany-13 <zambranocamcachoyirleyalejandra@gmail.com>
2026-08-10 14:26:57 -0600

    iconos

--- src/view/home/index.jsx
+++ src/view/home/index.jsx
@@ -1,19 +1,23 @@
 import Contenst from "./contents";
-import * as Icons from "lucide-react";
+import { Truck, Shield, RefreshCw, Headphones } from "lucide-react";
 export default function Home() {
   return (
     <div>
       <div className="hero">
         <div className="tarjetas">
+          <Truck size={32} />
           <h1>ENVIOS RAPIDOS</h1>
         </div>
         <div className="tarjetas">
+          <Shield size={32} />
           <h1>COMPRAS SEGURAS</h1>
         </div>
         <div className="tarjetas">
+          <RefreshCw size={32} />
           <h1>DEVOLUCIONES FACILES</h1>
         </div>
         <div className="tarjetas">
+          <Headphones size={32} />
           <h1>ATENCION AL CLIENTE</h1>
         </div>
       </div>
